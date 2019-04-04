04625e7f1a7c7045b2932ab96763fa1ccafccbac
Spencer West <spencer.west@kbb.com>
2019-04-04 09:00:45 -0700

    updates

--- content_script.js
+++ content_script.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "testScript.js"});
+$(document).ready(function(){
+  var experimentID = $('div[data-test-section="experiment-id"]')[0].textContent;
+  var variations = $('div[data-test-section="variations-card"]').find('div[data-test-section]');
+  variations.each(function() {
+    var string = this.getAttribute('data-test-section');
+    var arr = string.split('-');
+    arr = arr.pop();
+    console.log('('+experimentID+'):('+arr+')');
+  })
 });
